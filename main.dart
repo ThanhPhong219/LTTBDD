7673103ade6379368730450d06554904d2a7912d
ThanhPhong219 <85099798+ThanhPhong219@users.noreply.github.com>
2023-04-26 15:31:18 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -32,11 +32,56 @@ class MyApp extends StatelessWidget{
         Row(
           SizedBox(width:15,),
           children :[
-          Text("Shop", style : TextStyle(fontSize26),),
-          Text("Application", style : TextStyle(fontSize26),fontWeight:FontWeight.bold),
+          Text("Shop", style : TextStyle(fontSize: 32,letterSpacing: 1),),
+          Text("Application", style : TextStyle(fontSize: 32,fontWeight:FontWeight.bold,letterSpacing: 1),
           ],
-      ),],),
+      ),
+        SizedBox(height:20,)
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children:[
+            buildFlatButton("home Decore"),
+            buildFlatButton("Bath & Body"),
+            buildFlatButton("Beauty"),
+          ],
+      ),
+        SizedBox(
+          height:20,
+        )
+        Expanded(
+          child: Container(
+            color: Colors.white,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(40),),
+              boxShadow:[
+                BoxShadow(
+                  color: Colors.grey[300],
+                  spreadRadius: 1,
+                  blurRadius:20,
+                )
+              ]
+            )
+        ),
+        )
+      ],
+      ),
       ),
     );
+}
+FlatButton buildFlatButton(String text,{bool isSelected=false}){
+  return FlatButton(
+            onPressed: (){
+              print("Button pressed");
+            },
+            child: Text(
+              text, 
+              style: TextStyle(color: isSelected ? Colors.white:Colors.black,
+              fontSize:18
+              ),
+            ),
+            shape: StadiumBorder(),
+            color: isSelected ? Colors.pink[100]:Colors.grey[400],
+          );
 }
 }
